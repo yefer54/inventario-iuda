6d38b11533d099981aa15ed0fc4279722a2101a1
yefer54 <villotapantojay@gmail.com>
2025-12-09 23:31:30 -0500

    first commit

---
+++ src/main/java/com/iudigital/inventario_iuda/InventarioIudaApplication.java
@@ -0,0 +1,13 @@
+package com.iudigital.inventario_iuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioIudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioIudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iudigital/inventario_iuda/InventarioIudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iudigital.inventario_iuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioIudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
